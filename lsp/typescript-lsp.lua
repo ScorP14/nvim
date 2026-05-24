2f64e77ca5f474f6c3f9d38940754b5fd2adfd42
ScorP14 <i3@vm.home>
2026-05-24 15:35:01 +0800

    add typescript-lsp

---
+++ lsp/typescript-lsp.lua
@@ -0,0 +1,7 @@
+return {
+  name = "JS",
+  cmd = { 'typescript-language-server', "--stdio" },
+  filetypes = { 'typescriptreact', "typescript", 'javascript' },
+  root_markers = { '.git' },
+  settings = {},
+}
