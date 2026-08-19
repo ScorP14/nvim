c066808cc4825ed31117de76d8775a0f7dfd46aa
Termux <Termux@phone>
2026-08-19 09:48:42 +0800

    edit lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -7,7 +7,7 @@ return {
   opts = {
     options = {
       icons_enabled = true,
-      theme = "dracula", --dracula, gruvbox, onedark, tokyonignt
+      theme = "auto", --dracula, gruvbox, onedark, tokyonignt
     },
     sections = {
       lualine_a = { "mode" },
